e6cc349e11231c8e9d6024ae36d52f97cb68edf7
Leonel Balduino <leonel.balduino@hotmail.com>
2024-05-20 23:50:02 -0300

    Adding comments to the posts

--- src/main/java/com/leonel/workshopmong/config/Instantiation.java
+++ src/main/java/com/leonel/workshopmong/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.leonel.workshopmong.DTO.AuthorDTO;
+import com.leonel.workshopmong.DTO.CommentDTO;
 import com.leonel.workshopmong.domain.Post;
 import com.leonel.workshopmong.domain.User;
 import com.leonel.workshopmong.repository.PostRepository;
@@ -42,8 +43,15 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post (null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje.", new AuthorDTO(maria));
 		
-		postRepository.saveAll(Arrays.asList(post1, post2));
+		CommentDTO c1 = new CommentDTO("Boa viagem", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Boa viagem", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c3 = new CommentDTO("Boa viagem", sdf.parse("21/03/2018"), new AuthorDTO(alex));		
 		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+			
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		repository.save(maria);
 	}
