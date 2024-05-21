5dd07b881515261c7ac226d37742719040f7e3e8
Leonel Balduino <leonel.balduino@hotmail.com>
2024-05-20 22:27:58 -0300

    Adding AuthorDTO and changing Post to instantiante AuthorDTO instead of User.

--- src/main/java/com/leonel/workshopmong/config/Instantiation.java
+++ src/main/java/com/leonel/workshopmong/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.leonel.workshopmong.DTO.AuthorDTO;
 import com.leonel.workshopmong.domain.Post;
 import com.leonel.workshopmong.domain.User;
 import com.leonel.workshopmong.repository.PostRepository;
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", maria);
-		Post post2 = new Post (null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje.", maria);
-				
+		//Se não salvarmos os usuários antes de salvar os posts, os usuários aninhados aos posts não terão seus ids corretamente associados
 		repository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+		Post post2 = new Post (null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje.", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
